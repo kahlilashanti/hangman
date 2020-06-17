59ddaceb26d9aee000e630d09fc95c177b2a6b9c
seetsdev <bushongo@protonmail.com>
2020-06-17 16:48:54 -0700

    notification dom settings updated

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const figureParts = document.querySelectorAll('.figure-part');
 //create an array of words
 const words = ['priority', 'barbecue', 'olives', 'vulture', 'beautiful', 'dinosaur'];
 
+
 //loop through the words and choose one and assign it to a variable
 const selectedWord = words[Math.floor(Math.random() * words.length)];
 
@@ -19,7 +20,7 @@ const selectedWord = words[Math.floor(Math.random() * words.length)];
 // console.log(selectedWord);
 
 //create array to store correct letters
-const correctLetters = ['w', 'o', 'a'];
+const correctLetters = [];
 
 //create array to store the incorrect letters
 const wrongLetters = [];
@@ -41,6 +42,58 @@ function displayWord() {
     //get rid of hard return after each character so they can display on each line
     const innerWord = wordEl.innerText.replace(/\n/g, '');
     // console.log(wordEl.innerText, innerWord);
+
+    if (innerWord === selectedWord) {
+        finalMessage.innerText = 'Congratulations! You Won! 😀';
+        popup.style.display = 'flex';
+    }
 }
+
+//update wrong lettes element
+function updateWrongLettersEl() {
+    console.log('update wrong')
+}
+
+//show notification
+function showNotification() {
+    //add and then remove 'show' class
+    notification.classList.add('show');
+
+    //we want it to disappear after a certain amount of time
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
+}
+
+
+//keydown letter press
+window.addEventListener('keydown', e => {
+    //letters only
+    //keycodes are A(65) to Z(90)
+    // console.log(e.keyCode);
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        // console.log('its working')
+        const letter = e.key;
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord();
+            } else {
+                showNotification();
+            }
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+
+                //then display wrong letters in DOM
+                updateWrongLettersEl();
+            } else {
+                showNotification();
+            }
+        }
+    }
+});
 
 displayWord()
